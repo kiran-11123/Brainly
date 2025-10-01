285feef108625a31e851bb889a32ec6a1196efcd
Kiran <kiran.sadara6730@gmail.com>
2025-10-01 10:45:36 +0530

    created button component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import { Button } from './components/ui/Button'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div>
-      <h1 className='text-center bg-red-400'>Hello</h1>
+
+      <Button 
+         variant="primary" 
+         startIcon="+"
+        text="abc" 
+        size="md" 
+        onClick={() => alert("Button clicked!")} 
+        />
     </div>
   )
 }
