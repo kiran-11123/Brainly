4fee94ad47d6a79e4691cb703e63d13599782d19
Kiran <kiran.sadara6730@gmail.com>
2025-10-02 12:32:38 +0530

    frontend started again

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import { Button } from './components/ui/Button'
+import { Button } from './components/Button'
 import { PlusIcon } from './icons/PlusIcon'
 import { ShareIcon } from './icons/ShareIcon'
 
@@ -8,33 +8,12 @@ function App() {
 
 
   return (
-    <div className='flex  min-h-screen'>
-
-      <div className='w-72 px-4 py-2 bg-white shadow-2xl  flex flex-col'>
-
-        <div className='text-center font-sans font-bold text-xl'>
-
-          Second Brain
-          
-        </div>
-
-      </div>
-
-      <div className='flex-1 flex  flex-col  p-4 bg-gray-100 shadow-xl '>
-
-       
-        <div className='flex items-center justify-between mt-5'>
-
-          <div className='text-xl font-sans font-semibold'>
-            All Notes
-          </div>
-
-          <div className='flex gap-2'>
-
-      
 
 
-            <Button 
+      <div>
+
+
+      <Button 
          variant="secondary" 
          startIcon={<ShareIcon  size={"md"} /> }
         text="Share Brain" 
@@ -50,18 +29,9 @@ function App() {
         onClick={() => alert("Button clicked!")} 
         />
           
-        </div>
-
-      </div>
-
-        <div className='flex-1'>
-
-        </div>
-
-
     </div>
 
-    </div>
+ 
     
   )
 }
